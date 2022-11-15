fcd42ee8c4a30f82b1c6ca1f2ecbb30e66276d3c
cuitwangshicheng <775799488@qq.com>
2022-11-15 14:01:48 +0800

    新增登录页面

---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<!--
+name:
+creator:CUIT_MOON
+create-time:2022-11-15
+-->
+<template>
+  <div class="login-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login-page',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .login-page{
+    position:relative;
+    width:100%;
+    height:100%;
+  }
+</style>
